8e3525bc743869ca8d95a7e7bf147e624e3b5ecd
iamrajeev2005 <fea.rajeev@gmail.com>
2024-02-03 13:49:23 +0530

    locomotive added

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
+function locomotive() {
+  gsap.registerPlugin(ScrollTrigger);
+
+  // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+  const locoScroll = new LocomotiveScroll({
+    el: document.querySelector(".main"),
+    smooth: true,
+  });
+  // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+  locoScroll.on("scroll", ScrollTrigger.update);
+
+  // tell ScrollTrigger to use these proxy methods for the ".main" element since Locomotive Scroll is hijacking things
+  ScrollTrigger.scrollerProxy(".main", {
+    scrollTop(value) {
+      return arguments.length
+        ? locoScroll.scrollTo(value, 0, 0)
+        : locoScroll.scroll.instance.scroll.y;
+    }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+    getBoundingClientRect() {
+      return {
+        top: 0,
+        left: 0,
+        width: window.innerWidth,
+        height: window.innerHeight,
+      };
+    },
+    // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+    pinType: document.querySelector(".main").style.transform
+      ? "transform"
+      : "fixed",
+  });
+  // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll.
+  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+  // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+  ScrollTrigger.refresh();
+}
 function loaderAnim() {
   var tl = gsap.timeline();
   tl.from(".loader .text h1,.loader .text h2", {
@@ -112,13 +150,6 @@ function videoCrsr() {
   });
 }
 
-Shery.makeMagnet(".page1 nav .nav-1 .svg svg,.page1 nav .nav-2 a");
-
-loaderAnim();
-crsrAnim();
-heroAnim();
-videoCrsr();
-
 function heroAnim() {
   const flagImg = document.querySelector("#flag");
   document.addEventListener("mousemove", function (e) {
@@ -138,3 +169,11 @@ function heroAnim() {
     });
   });
 }
+
+Shery.makeMagnet(".page1 nav .nav-1 .svg svg,.page1 nav .nav-2 a");
+
+loaderAnim();
+crsrAnim();
+heroAnim();
+locomotive();
+videoCrsr();
